905c0ca00519522c1663114aa9332d1d8baa9c25
Oleksandr Yakushev <alex@bytopia.org>
2024-10-15 18:08:14 +0300

    s

--- src/flamebin/storage.clj
+++ src/flamebin/storage.clj
@@ -1,16 +1,17 @@
 (ns flamebin.storage
-  (:require [clojure.java.io :as io])
-  (:import java.io.ByteArrayInputStream)
-  )
+  (:require [clojure.java.io :as io]
+            [omniconf.core :as cfg])
+  (:import java.io.ByteArrayInputStream))
 
-(def root-dir (doto (io/file "storage") .mkdirs))
+(defn root-dir []
+  (cfg/get :storage :path))
 
 (defn save-file [content filename]
-  (with-open [f (io/output-stream (io/file root-dir filename))]
+  (with-open [f (io/output-stream (io/file (root-dir) filename))]
     (io/copy content f)))
 
 (defn get-cpf-file [profile-id]
-  (io/file root-dir (format "%s.cpf" profile-id)))
+  (io/file (root-dir) (format "%s.cpf" profile-id)))
 
 (defn get-file [path]
-  (io/file root-dir path))
+  (io/file (root-dir) path))
